8d58a1b74e7c7ef42b695b04ae4ad44f4732c48f
니꼬 <nico@nomadcoders.co>
2023-08-14 17:19:07 +0900

    1.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANd4yZT54bSK0AcbItj1bqB7q5x78fOT0",
+  authDomain: "nwitter-reloaded.firebaseapp.com",
+  projectId: "nwitter-reloaded",
+  storageBucket: "nwitter-reloaded.appspot.com",
+  messagingSenderId: "597055701730",
+  appId: "1:597055701730:web:ad1f4784fbb491e2a614bb",
+};
+
+const app = initializeApp(firebaseConfig);
